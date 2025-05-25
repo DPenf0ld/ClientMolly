517fd6283e6fa427cc6c58de0bec38e76e6bd7bb
DPenf0ld <dmpenfold100@gmail.com>
2025-05-25 22:04:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+const buttons = document.querySelectorAll(".btn");
+
+
+buttons.forEach((btn) => {
+  btn.addEventListener('mousemove', (e) => {
+    const rect = btn.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+
+    btn.style.setProperty('--x', x + 'px');
+    btn.style.setProperty('--y', y + 'px');
+  });
+});
+
+const headerOffset = 100; // Adjust this to your header's height
+
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const targetId = button.getAttribute('data-target');
+    if (targetId === 'top') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      const target = document.getElementById(targetId);
+      if (target) {
+        const elementPosition = target.getBoundingClientRect().top + window.pageYOffset;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+        });
+      }
+    }
+  });
+});
+
+
+
+const countdownDate = new Date("2025-06-31T23:59:59").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  if (distance < 0) {
+    document.getElementById("timer").innerHTML = "The countdown is over!";
+    clearInterval(timerInterval);
+    return;
+  }
+
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+ // document.getElementById("timer").innerHTML =
+ //   days +
+ //   "d " +
+  //  hours +
+  //  "h " +
+   // minutes +
+   // "m " +
+  //  seconds +
+   // "s ";
+
+  document.getElementById("timer").innerHTML ="PENDING";
+
+}
+
+// Update countdown every second
+const timerInterval = setInterval(updateCountdown, 1000);
+updateCountdown(); // call
+
+
+
+const toggle = document.getElementById("onoff");
+
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
+
+
+
+
